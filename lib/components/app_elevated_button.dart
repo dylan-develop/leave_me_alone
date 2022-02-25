8ee3b361626bce56b7fd069472686748f7b1a9b3
dylan <development.dylan@gmail.com>
2022-02-25 15:21:17 +0800

    Update onboard page layout

--- lib/components/app_elevated_button.dart
+++ lib/components/app_elevated_button.dart
@@ -31,95 +31,99 @@ class _AppElevatedButtonState extends State<AppElevatedButton> {
   Widget build(BuildContext context) {
     final _isDesktop = MediaQuery.of(context).size.width > 1024;
 
-    return Stack(
-      clipBehavior: Clip.none,
-      children: [
-        Positioned(
-          left: _isDesktop ? 8 : 4,
-          top: _isDesktop ? 8 : 4,
-          child: Container(
-            decoration: BoxDecoration(
-              color: Colors.black,
-              borderRadius: BorderRadius.circular(8),
+    return SizedBox(
+      width: widget.width,
+      height: widget.height,
+      child: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          Positioned(
+            left: _isDesktop ? 8 : 4,
+            top: _isDesktop ? 8 : 4,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.circular(8),
+              ),
+              width: widget.width,
+              height: widget.height,
             ),
-            width: widget.width,
-            height: widget.height,
           ),
-        ),
-        AnimatedPositioned(
-          duration: const Duration(milliseconds: 200),
-          left: _onHover
-              ? _isDesktop
-                  ? 8
-                  : 4
-              : 0,
-          top: _onHover
-              ? _isDesktop
-                  ? 8
-                  : 4
-              : 0,
-          child: MouseRegion(
-            cursor: SystemMouseCursors.click,
-            onEnter: (event) {
-              setState(() {
-                _onHover = true;
-              });
-              widget.onHover?.call(true);
-            },
-            onExit: (event) {
-              setState(() {
-                _onHover = false;
-              });
-              widget.onHover?.call(false);
-            },
-            child: GestureDetector(
-              onTap: () {
-                widget.onTap.call();
-              },
-              onTapDown: (event) {
+          AnimatedPositioned(
+            duration: const Duration(milliseconds: 200),
+            left: _onHover
+                ? _isDesktop
+                    ? 8
+                    : 4
+                : 0,
+            top: _onHover
+                ? _isDesktop
+                    ? 8
+                    : 4
+                : 0,
+            child: MouseRegion(
+              cursor: SystemMouseCursors.click,
+              onEnter: (event) {
                 setState(() {
                   _onHover = true;
                 });
                 widget.onHover?.call(true);
               },
-              onTapUp: (event) {
+              onExit: (event) {
                 setState(() {
                   _onHover = false;
                 });
                 widget.onHover?.call(false);
               },
-              onTapCancel: () {
-                setState(() {
-                  _onHover = false;
-                });
-                widget.onHover?.call(false);
-              },
-              child: Container(
-                width: widget.width,
-                height: widget.height,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(8),
-                  border: Border.all(
-                    color: Colors.black,
-                    width: _isDesktop ? 2 : 1,
-                  ),
-                ),
-                child: Center(
-                  child: Text(
-                    widget.title,
-                    style: TextStyle(
-                      fontSize: widget.fontSize,
-                      fontFamily: widget.fontFamily,
+              child: GestureDetector(
+                onTap: () {
+                  widget.onTap.call();
+                },
+                onTapDown: (event) {
+                  setState(() {
+                    _onHover = true;
+                  });
+                  widget.onHover?.call(true);
+                },
+                onTapUp: (event) {
+                  setState(() {
+                    _onHover = false;
+                  });
+                  widget.onHover?.call(false);
+                },
+                onTapCancel: () {
+                  setState(() {
+                    _onHover = false;
+                  });
+                  widget.onHover?.call(false);
+                },
+                child: Container(
+                  width: widget.width,
+                  height: widget.height,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8),
+                    border: Border.all(
+                      color: Colors.black,
+                      width: _isDesktop ? 2 : 1,
                     ),
-                    textAlign: TextAlign.center,
+                  ),
+                  child: Center(
+                    child: Text(
+                      widget.title,
+                      style: TextStyle(
+                        fontSize: widget.fontSize,
+                        fontFamily: widget.fontFamily,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
                   ),
                 ),
               ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
--- lib/screens/onboard.dart
+++ lib/screens/onboard.dart
@@ -1,7 +1,9 @@
+import 'dart:math';
+
 import 'package:beamer/beamer.dart';
 import 'package:flutter/material.dart';
+import 'package:leave_me_alone/components/app_elevated_button.dart';
 import 'package:leave_me_alone/components/character_block.dart';
-import 'package:leave_me_alone/components/elevated_button.dart';
 import 'package:leave_me_alone/components/responsive_layout_builder.dart';
 
 class OnboardPage extends StatelessWidget {
@@ -10,109 +12,199 @@ class OnboardPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Container(
-                margin: const EdgeInsets.symmetric(vertical: 8),
-                child: const ResponsiveLayoutBuilder(
-                  mobile: Text(
-                    'LEAVE ME ALONE',
-                    style: TextStyle(
-                      fontFamily: 'ThinkBig',
-                      fontSize: 48,
-                    ),
-                    textAlign: TextAlign.center,
+      body: ResponsiveLayoutBuilder(
+        mobile: Container(
+          margin: const EdgeInsets.symmetric(
+            horizontal: 32,
+          ),
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: const [
+                  _OnboardTitle(
+                    fontSize: 48,
                   ),
-                  desktop: Text(
-                    'LEAVE ME ALONE',
-                    style: TextStyle(
-                      fontFamily: 'ThinkBig',
-                      fontSize: 96,
-                    ),
-                    textAlign: TextAlign.center,
+                  _OnboardSubtitle(
+                    fontSize: 24,
                   ),
-                ),
-              ),
-              Container(
-                margin: const EdgeInsets.symmetric(vertical: 8),
-                child: const ResponsiveLayoutBuilder(
-                  mobile: Text(
-                    'a stupid game',
-                    style: TextStyle(
-                      fontFamily: 'HandWriting',
-                      fontSize: 24,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                  desktop: Text(
-                    'a stupid game',
-                    style: TextStyle(
-                      fontFamily: 'HandWriting',
-                      fontSize: 36,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-              Center(
-                child: Container(
-                  margin: const EdgeInsets.symmetric(
-                    vertical: 64,
-                  ),
-                  child: ConstrainedBox(
-                    constraints: const BoxConstraints(
-                      minWidth: 272,
-                    ),
-                    child: SizedBox(
-                      width: MediaQuery.of(context).size.width * 0.3,
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          const Expanded(
-                            child: CharacterBlock(
-                              shakeOnHover: true,
-                              imageUrl: 'assets/images/charactera.png',
-                            ),
-                          ),
-                          ResponsiveLayoutBuilder(
-                            mobile: Container(width: 32),
-                            desktop: Container(width: 80),
-                          ),
-                          const Expanded(
-                            child: CharacterBlock(
-                              shakeOnHover: true,
-                              imageUrl: 'assets/images/characterb.png',
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              Center(
-                child: ConstrainedBox(
-                  constraints: const BoxConstraints(
+                  _CharactersRow(
                     minWidth: 264,
                   ),
-                  child: SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.3,
-                    child: CustomElevatedButton(
-                      title: 'start now',
-                      onPressed: () {
-                        context.beamToNamed('/difficulties');
-                      },
-                    ),
+                  _OnboardStartButton(
+                    fontSize: 24,
+                    minWidth: 264,
                   ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        desktop: Container(
+          padding: EdgeInsets.symmetric(
+            vertical: 128 * MediaQuery.of(context).size.height / 1024,
+          ),
+          child: Row(
+            children: [
+              Expanded(
+                flex: 3,
+                child: Container(),
+              ),
+              Expanded(
+                flex: 4,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: const [
+                    _OnboardTitle(
+                      fontSize: 96,
+                    ),
+                    _OnboardSubtitle(
+                      fontSize: 36,
+                    ),
+                    Flexible(
+                      child: _CharactersRow(),
+                    ),
+                    _OnboardStartButton(
+                      fontSize: 36,
+                    ),
+                  ],
                 ),
-              )
+              ),
+              Expanded(
+                flex: 3,
+                child: Container(),
+              ),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class _OnboardTitle extends StatelessWidget {
+  final double fontSize;
+
+  const _OnboardTitle({
+    Key? key,
+    required this.fontSize,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'LEAVE ME ALONE',
+      style: TextStyle(
+        fontFamily: 'ThinkBig',
+        fontSize: fontSize,
+      ),
+      textAlign: TextAlign.center,
+    );
+  }
+}
+
+class _OnboardSubtitle extends StatelessWidget {
+  final double fontSize;
+
+  const _OnboardSubtitle({
+    Key? key,
+    required this.fontSize,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'a stupid game',
+      style: TextStyle(
+        fontFamily: 'HandWriting',
+        fontSize: fontSize,
+      ),
+      textAlign: TextAlign.center,
+    );
+  }
+}
+
+class _CharactersRow extends StatelessWidget {
+  final double? minWidth;
+
+  const _CharactersRow({
+    Key? key,
+    this.minWidth,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Center(
+          child: Container(
+            margin: EdgeInsets.symmetric(
+              vertical: 72 * MediaQuery.of(context).size.height / 1024,
+              horizontal: 24
+            ),
+            width: minWidth == null
+                ? constraints.maxWidth
+                : min(minWidth!, constraints.maxWidth),
+            child: Row(
+              children: [
+                const Expanded(
+                  flex: 8,
+                  child: CharacterBlock(
+                    shakeOnHover: true,
+                    imageUrl: 'assets/images/charactera.png',
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(),
+                ),
+                const Expanded(
+                  flex: 8,
+                  child: CharacterBlock(
+                    shakeOnHover: true,
+                    imageUrl: 'assets/images/characterb.png',
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class _OnboardStartButton extends StatelessWidget {
+  final double fontSize;
+  final double? minWidth;
+
+  const _OnboardStartButton({
+    Key? key,
+    required this.fontSize,
+    this.minWidth,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          return AppElevatedButton(
+            width: minWidth == null
+                ? constraints.maxWidth
+                : min(minWidth!, constraints.maxWidth),
+            height: fontSize * 1.5,
+            title: 'start now',
+            fontSize: fontSize,
+            onTap: () {
+              context.beamToNamed('/difficulties');
+            },
+          );
+        },
       ),
     );
   }
